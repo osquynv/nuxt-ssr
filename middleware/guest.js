56b66a3c31412c7bd84e75ae5c1d91b7c46ed6f0
Quý <quynv@anvui.vn>
2020-05-14 17:54:08 +0700

    create middleware guest

---
+++ middleware/guest.js
@@ -0,0 +1,7 @@
+export default function (context) {
+    console.log(1);
+    // If the user is not authenticated
+    // if (!store.state.authenticated) {
+    //     return redirect('/login')
+    // }
+}
