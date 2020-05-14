4f7d440c19a4eae2ba1c125db230eaa28b7d3c97
Quý <quynv@anvui.vn>
2020-05-14 17:30:06 +0700

    init

---
+++ store/index.js
@@ -0,0 +1,15 @@
+export const state = () => ({
+    counter: 0
+})
+
+export const mutations = {
+    increment (state) {
+        state.counter++
+    }
+}
+export const actions = {
+    async nuxtServerInit (context) {
+        context.commit('increment');
+    }
+};
+
